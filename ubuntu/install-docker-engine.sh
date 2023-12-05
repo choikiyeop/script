2824ef2583474ecaacd516c2083c598bcc51ad0a
choikiyeop <cky9709@naver.com>
2023-12-05 17:56:18 +0900

    docker-engine install -y 추가

--- ubuntu/install-docker-engine.sh
+++ ubuntu/install-docker-engine.sh
@@ -6,7 +6,7 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 
 echo "Add Docker's official GPG key"
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -19,5 +19,5 @@ echo \
 sudo apt-get update
 
 echo "Install the Docker packages"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
