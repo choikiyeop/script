5fafa5a20bc1387d6c8328def69bed75fe9519ef
choikiyeop <cky9709@naver.com>
2023-12-05 17:43:22 +0900

    워커노드 설치

---
+++ ubuntu/install-worker-v1.28.sh
@@ -0,0 +1,40 @@
+o "installing kubeadm (Kubernetes v1.28) master node"
+
+echo "Swap off"
+sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
+
+echo "Forwarding IPv4 and letting iptables see bridged traffic"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+echo "Preflight error fix"
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+
+echo "Installing kubeadm, kubelet and kubectl"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# join 명령어 입력
