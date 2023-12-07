11f137fe4f4c1d863a5c2a47c964b19b3cbe4396
choikiyeop <127378501+choikiyeop@users.noreply.github.com>
2023-12-07 13:34:49 +0900

    install-k8s-master의 addon전에 sleep 추가

--- ubuntu/install-master-v1.28.sh
+++ ubuntu/install-master-v1.28.sh
@@ -44,6 +44,7 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo "Installing Addon"
+sleep 30s
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
 
