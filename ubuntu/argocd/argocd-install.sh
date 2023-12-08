e8ae1666ce4f1c8bc04b0ff088c80d32d08efb87
choikiyeop <127378501+choikiyeop@users.noreply.github.com>
2023-12-08 14:20:05 +0900

    Argo CD 설치 스크립트 수정

--- ubuntu/argocd/argocd-install.sh
+++ ubuntu/argocd/argocd-install.sh
@@ -1,7 +1,17 @@
 # https://argo-cd.readthedocs.io/en/stable/getting_started/
 
-echo "Install Argo CD"
+# Install Argo CD
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# kubectl port-forward svc/argocd-server -n argocd 8080:443
+# Download Argo CD CLI
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+# Expose external IP
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+
+# Login Using The CLI
+echo "show initial password!"
+argocd admin initial-password -n argocd
