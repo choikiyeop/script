43e4cca27a872745c25da0144654072beac3df5c
choikiyeop <127378501+choikiyeop@users.noreply.github.com>
2023-12-14 18:26:25 +0900

    systemcgroup 설정 수정

--- ubuntu/kubernetes/master-v1.29-install.sh
+++ ubuntu/kubernetes/master-v1.29-install.sh
@@ -5,6 +5,7 @@ echo "containerd cgroup setting"
 # containerd config default | sudo tee /etc/containerd/config.toml
 # vi /etc/containerd/config.toml에서 SystemdCgroup = true로 바꾸어 주어야 한다.
 # systemctl restart containerd
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
 echo "Swap off"
 sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
