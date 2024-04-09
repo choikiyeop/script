90cf26cefc668829e052bc1ba74c9a9c68958215
choikiyeop <cky9709@naver.com>
2024-04-09 03:48:57 +0000

    오라클 클라우드 환경 설정 추가

---
+++ ubuntu/kubernetes/oracle-cloud/control-plane-v1.29-install.sh
@@ -0,0 +1,23 @@
+# 기본 오라클 클라우드 인스턴스 설정
+sudo iptables-save > ~/iptables-rules
+
+grep -v "DROP" iptables-rules > tmpfile && mv tmpfile iptables-rules-mod
+grep -v "REJECT" iptables-rules-mod > tmpfile && mv tmpfile iptables-rules-mod
+
+sudo iptables-restore < ~/iptables-rules-mod
+
+sudo iptables -L
+
+sudo netfilter-persistent save
+sudo systemctl restart iptables
+
+sudo ufw disable
+
+# /etc/hosts에서 아래 추가(프라이빗 IP, 퍼블릭 IP)
+# 10.0.14.71		144.24.90.0
+
+# docker 설치
+
+# 기본 설정
+
+# kubeadm install
